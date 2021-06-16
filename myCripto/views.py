b8520f200a07fa615bd8319da51ac48dfeba69d9
aidalaida <aidagilconde@gmail.com>
2021-06-16 22:25:24 +0200

    Dando estilos con css

--- myCripto/views.py
+++ myCripto/views.py
@@ -1,5 +1,19 @@
 from myCripto import app
+from flask import render_template
+
 
 @app.route('/')
-def index():
-    return 'Flask rula'
+def listaMovimientos():
+    return render_template('inicio.html')
+
+@app.route('/compra')
+def comprar():
+    return render_template('comprar.html')
+
+@app.route('/status')
+def status():
+    return render_template('status.html')
+
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
