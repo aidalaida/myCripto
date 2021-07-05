db0c2ec9fe717f91e8cd3f81b3ad29659b8eec41
aidalaida <aidagilconde@gmail.com>
2021-07-05 17:44:10 +0200

    acabado

--- config_template.py
+++ config_template.py
@@ -1,6 +1,4 @@
-from config import API_KEY
-
 
 SECRET_KEY= <clave secreta para WTF>
-DATABASE= <ruta a la base de datos>
+DATABASE= <ruta de acceso a la base de datos>
 API_KEY= <clave secreta para la API>
